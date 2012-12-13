dc0faafa91e9717628f32b9f70f09f36546a627e
Bas Bootsma <bootsman@gmail.com>
2012-12-13 20:13:19 +0100

    Added arm moving.

--- webots/naoarmmoving/controllers/NaoArmMoving/NaoArmMoving.java
+++ webots/naoarmmoving/controllers/NaoArmMoving/NaoArmMoving.java
@@ -16,11 +16,8 @@ public class NaoArmMoving extends Robot
 {
   public static final int STEP_TIME = 64;
   public static final int MILLISECONDS_IN_SECOND = 1000;
-    
-  public static final String DATA_FILE_PATH = "../../../../data/accelerometer.csv";
-  
+      
   private Motion motion;
-  private Accelerometer accelerometer;
   
   private double timeElapsed;
   
@@ -31,43 +28,21 @@ public class NaoArmMoving extends Robot
   {
     super();
     
-    this.motion = new Motion( "../../../../data/generations/ForwardsEvolved.motion" );
-    
-    // Enable accelerometer.
-    this.accelerometer = this.getAccelerometer( "Accelerometer" ); 
-    this.accelerometer.enable( NaoArmMoving.STEP_TIME );
+    this.motion = new Motion( "../../../../data/generations/ArmMoving.motion" );
   }
   public void run()
   {
     // Create file for writing.
     try
     {
-      FileWriter fileWriter = new FileWriter( new File( NaoArmMoving.DATA_FILE_PATH ) );
-      PrintWriter printWriter = new PrintWriter( fileWriter );
-      
-      // Write header.
-      //printWriter.printf( "time;x;y;z\n" );
-      
       // Main loop.
       do
       {
         this.motion.play();
         
-        // Write accelerometer values.
-        double[] values = this.accelerometer.getValues();
-        
-        printWriter.printf( Locale.US,
-                            "%f;%f;%f;%f\n",
-                            timeElapsed,
-                            XXX );
-        
         timeElapsed += (double)NaoArmMoving.STEP_TIME / NaoArmMoving.MILLISECONDS_IN_SECOND;
       }
       while( this.step( NaoArmMoving.STEP_TIME ) != -1 );
-      
-      // Close writer.
-      printWriter.close();
-      fileWriter.close();
     }
     catch( Exception e )
     {
