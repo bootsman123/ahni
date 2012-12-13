3ac9cc37cd4ff5774944877cb7392a97fbce5f47
Tom de Ruijter <t.deruijter@student.ru.nl>
2012-12-13 15:46:34 +0100

    Changed build options to Java 1.6 for Ant. Added derivative based fitness calculation for arm movement.

--- src/ojc/ahni/experiments/naoarmmoving/Fitnessizer.java
+++ src/ojc/ahni/experiments/naoarmmoving/Fitnessizer.java
@@ -2,9 +2,8 @@ package ojc.ahni.experiments.naoarmmoving;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import ojc.ahni.experiments.nao.CSVReader;
 
@@ -12,73 +11,43 @@ public class Fitnessizer
 {
 	private String dataFilePath;
 	private CSVReader dataReader;
-	private Map<Double, Double> lookupValuesX;
-	private Map<Double, Double> lookupValuesY;
-	private Map<Double, Double> lookupValuesZ;
+	private List<Double> lookupValuesH;
 	
 	public Fitnessizer( String dataFilePath ) throws FileNotFoundException, IOException
 	{
 		this.dataFilePath = dataFilePath;
 
 		this.dataReader = new CSVReader( this.dataFilePath, ";" );
-
-		this.lookupValuesX = new HashMap<Double,Double>();
-		this.lookupValuesY = new HashMap<Double, Double>();
-		this.lookupValuesZ = new HashMap<Double, Double>();
-		
-		// Process values.
-		this.process();
-
+		this.lookupValuesH = diff(dataReader);		
 	}
 	
 	public Double calculate( String filePath ) throws FileNotFoundException, IOException
 	{
 		Double fitness = 0.0;
-		CSVReader reader = new CSVReader( filePath, ";" );
-		
-		// Calculate the fitness.
-		List<String> values;
-		while( ( values = reader.read() ) != null )
+		List<Double> newDiff = diff(new CSVReader( filePath, ";" ));
+		for (int i = 0; i < newDiff.size(); i++)
 		{
-			Double time = Double.valueOf( values.get( 0 ) );
-			Double x = Double.valueOf( values.get( 1 ) );
-			Double y = Double.valueOf( values.get( 2 ) );
-			Double z = Double.valueOf( values.get( 3 ) );
-			
-			// Get values from the lookup-table.
-			Double dataX = this.lookupValuesX.get( time );
-			Double dataY = this.lookupValuesY.get( time );
-			Double dataZ = this.lookupValuesZ.get( time );
-			
-			if( dataX == null || dataY == null || dataZ == null )
-			{
-				continue;
-			}
-			
-			fitness += Math.pow( dataX - x, 2 );
-			fitness += Math.pow( dataY - y, 2 );
-			fitness += Math.pow( dataZ - z, 2 );
+			fitness += Math.pow(newDiff.get(i) - this.lookupValuesH.get(i), 2);
 		}
 		
 		return fitness;
 	}
-
-	private void process() throws IOException
+	
+	private List<Double> diff (CSVReader r) throws IOException
 	{
-		List<String> values;
+		List<String> values = r.read(); //Read first line
+		Double t_ = Double.valueOf( values.get( 0 ) );
+		Double h_ = Double.valueOf( values.get( 1 ) );
 		
-		while( ( values = this.dataReader.read() ) != null )
+		List<Double> d = new ArrayList<Double>();
+		while( ( values = r.read() ) != null )
 		{
-			Double time = Double.valueOf( values.get( 0 ) );
-			Double x = Double.valueOf( values.get( 1 ) );
-			Double y = Double.valueOf( values.get( 2 ) );
-			Double z = Double.valueOf( values.get( 3 ) );
-
-			this.lookupValuesX.put( time, x );
-			this.lookupValuesY.put( time, y );
-			this.lookupValuesZ.put( time, z );
-			
-			//System.out.printf( "time: %f - x: %f - y: %f - z: %f\n", time, x, y, z );
+			Double t = Double.valueOf( values.get( 0 ) );
+			Double h = Double.valueOf( values.get( 1 ) );
+			d.add((h - h_) / (t - t_));
+			t_ = t;
+			h_ = h;
 		}
+		return d;
 	}
 }
